a7c19330a5c76489ff69d6efb9a40cd7a7a1b0a4
Nathanael Bubeck <nathanael@Mac.fritz.box>
2026-05-18 00:48:44 +0200

    Beschreibung deines Commits

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pgzrun
+import random
 
 WIDTH = 1398
 HEIGHT = 766
@@ -9,7 +10,9 @@ MAX_FALL_SPEED = 30
 vx = 0
 vy = 0
 
-platforms= []
+platforms = [Actor("platform0"),Actor("platform1"),Actor("platform2"),Actor("platform3"),Actor("platform0"),Actor("platform1"),Actor("platform2"),Actor("platform3"),
+]
+platform_pos_topleft = [(700,270),(900, -50), (1550,200),( 1900, 50),(300,220),(500, -10), (1500, -100),(1400, 300),(2000,220)]
 
 walk_frames = ["mage_walk1","mage_walk2",'mage_walk5',"mage_walk4"]
 idle_frames = ['mage_idle0','mage_idle1','mage_idle2','mage_idle3','mage_idle4']
@@ -32,6 +35,34 @@ mc = Actor('mage.png',midbottom=(704,623))
 mc.stand = True
 mc.on_g = True
 
+#platformen und orte (indexe) auswählen
+def platforminformation():
+    return random.sample(range(8), 5 )+ random.sample(range(9), 5)
+#platformen und positionen werden anhand von indexen rausgesucht und die platformen dann in eine liste zusammen gefügt
+def platformlist(list):
+    platform_draw_setup = []
+    
+    # 4 platformen pro stage
+    
+    for platform_index in range(5):
+        
+        #platform desing wird rausgesucht
+        actor = platforms[list[platform_index]]
+        
+        #platformposition wird zugeordnet
+        actor.topleft = platform_pos_topleft[list[platform_index + 5]]
+        
+        #platform wird an liste gefügt 
+        platform_draw_setup.append(actor)
+    
+    return platform_draw_setup 
+#lsite mit echten platformen erstelen
+platform_draw = []
+platform_draw = platformlist(platforminformation())
+#startposition der platformen merken
+platform_start_y = []
+for platform in platform_draw:
+    platform_start_y.append(platform.top)
 
 
 def draw():
@@ -42,6 +73,8 @@ def draw():
     #zeichne untergrund
     ground.draw()
     
+    for platform in platform_draw:
+        platform.draw() 
 
     
 
@@ -63,6 +96,8 @@ def update():
     if ground.x +vx  <= 1408 and ground.x + vx >= -10 and mc.x == 704:
         bg.x += vx / 2
         ground.x += vx
+        for platform in platform_draw:
+            platform.x += vx
     
     #moving mc
     elif mc.x - vx > 0 and mc.x - vx  < 1390:
@@ -73,7 +108,7 @@ def update():
         vy = -JUMP_SPEED
     vy = min(vy + GRAVITY, MAX_FALL_SPEED)
     
-    if vy >= 0:
+    if vy > 0:
         
         # Zielposition des Charakters (in der Luft)
         target = ground.top - vy
@@ -81,24 +116,50 @@ def update():
         # niedrigst mögliche Landeposition (Boden oder Plattform)
         landing_bottom = 0
         
+        over_platform = False
         # Plattformkollisionen überprüfen
-        for platform in platforms:
-            if (mc.right -85 > platform.left and mc.left + 93 < platform.right and mc.bottom -10  <= platform.top):
-                landing_bottom = min(landing_bottom, platform.top)
+        for platform in platform_draw:
+            if mc.right -85 > platform.left and mc.left + 93  < platform.right and mc.bottom - 10 <= platform.top + 60:
+                over_platform = True
+                #vergleichen ob man durch die platform fallen würden
+                differenz_y = min(platform.top - mc.bottom +70 , vy)
+                if differenz_y != vy:
+                    break
+        #wenn durch platform fallen würde, nur auf platform setzten
+        if over_platform == True and differenz_y != vy:
+            ground.top -= differenz_y
+            bg.top -= differenz_y/2
+            for platform in platform_draw:
+                platform.top -= differenz_y
+            mc.on_g = True
+            vy = 0
+            differenz_y = 0
 
-        if target <= landing_bottom:
+
+        #checken ob durch den boden fallen würde
+        elif target <= landing_bottom and over_platform == False:
             ground.top = 0
             bg.top = -300
+            count = 0
+            for platform in platform_draw:
+                platform.top = platform_start_y[count]
+                count +=1
             vy = 0 
             mc.on_g = True
+        
         else:
             ground.top = target
             bg.top = bg.top -vy/2
+            for platform in platform_draw:
+                platform.top -= vy
             mc.on_g = False
+    
     # y-Bewegung nach oben ausführen
     else:
         bg.top -= vy/2
         ground.top -= vy
+        for platform in platform_draw:
+            platform.top -= vy
         mc.on_g = False
 
     #animation walking
